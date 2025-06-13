ca1ae5bb0aed4741dcf5f62b19335e2592f5a78e
rika-chan05 <chirikavyra@gmail.com>
2025-06-13 22:33:12 +0700

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
     public function index() {
-        $data['content'] = '<h1>Welcome to Rumah Sakit Gangguan Jiwa</h1>';
+        $data['content'] = '<h1>Welcome to Rumah Sakit Harapan Semua</h1>';
         $this->load->view('templates/header');
         $this->load->view('templates/dashboard_admin', $data);
         $this->load->view('templates/footer');
